81a3f6cab8889325dec9479056ec4ba822faa0a0
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2021-11-18 13:50:22 +1000

    added is_admin field to User

--- uni_app/models/users.py
+++ uni_app/models/users.py
@@ -4,23 +4,15 @@ from werkzeug.security import check_password_hash
 
 class User(UserMixin, db.Model):
     __tablename__ = "flasklogin-users"
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    name = db.Column(
-        db.String(100),
-        nullable=False
-    )
-    email = db.Column(
-        db.String(40),
-        unique=True,
-        nullable=False
-    )
-    password = db.Column(
-        db.String(200),
-        nullable=False
-    )
+    id = db.Column(db.Integer, primary_key=True)
+    
+    name = db.Column(db.String(100), nullable=False)
+    
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    
+    password = db.Column(db.String(200), nullable=False)
+    
+    is_admin = db.Column(db.Boolean(), nullable=False, default=False)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
--- uni_app/schemas/user_schema.py
+++ uni_app/schemas/user_schema.py
@@ -13,6 +13,7 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
     id = auto_field(dump_only=True)
     name = auto_field(required=True, validate=validate.Length(1))
     email = auto_field(required=True, validate=validate.Email())
+    is_admin = auto_field(required=False, default=False)
     password = fields.Method(
         required=True,
         load_only=True,
